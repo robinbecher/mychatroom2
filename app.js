f83b2a0ae277eb57f8821f1d7c1ec771f92df67f
Robin Becher <robin.becher@gmail.com>
2018-05-29 21:17:46 +0200

    messages on connect and disconnect user can send their names

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var num = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    num++;
+    io.emit('chat message', 'Somebody has joined the chatroom!');
+    emitUserNumber();
+    socket.on('chat message', function(msg, name){
+    io.emit('chat message', name+': '+msg);
   });
+    socket.on('disconnect', function () {
+      io.emit('Somebody has left the chatroom!');
+      emitUserNumber();
+    })
+
 });
+
+function emitUserNumber() {
+    io.emit('there are currently '+num+' people in this chatroom.');
+};
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
