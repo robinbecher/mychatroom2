c899cf6477f19418d43daaf43b8164f7196e4b50
Robin Becher <robin.becher@gmail.com>
2018-05-30 15:21:06 +0200

    Added non-functional login screen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/indexOLD.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 var allClients=[];
