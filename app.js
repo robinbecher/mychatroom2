2f0324dee9a810ea38d595a652405c512c8236cb
Robin Becher <robin.becher@gmail.com>
2018-06-04 20:10:18 +0200

    fix for error?

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
-
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+// var bodyParser = require('body-parser')
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//     extended: true
+// }));
+//
+// app.use(express.json());       // to support JSON-encoded bodies
+// app.use(express.urlencoded()); // to support URL-encoded bodies
 
 
 app.get('/', function(req, res){
@@ -24,12 +24,12 @@ app.get('/files/avatars.png', function(req, res){
     res.sendFile(__dirname + '/files/avatars.png');
 });
 
-app.post('/chatroom.html', function (req,res) {
-    //TODO Pass name to html file????
-    var name = req.body.name;
-    res.send(name);
-    res.sendFile(__dirname + '/chatroom.html');
-})
+// app.post('/chatroom.html', function (req,res) {
+//     //TODO Pass name to html file????
+//     var name = req.body.name;
+//     res.send(name);
+//     res.sendFile(__dirname + '/chatroom.html');
+// })
 
 
 
