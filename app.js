493c49ed6911aa6de07e02b39afdb40319765257
Robin Becher <robin.becher@gmail.com>
2018-06-06 12:47:47 +0200

    try fix for missing css

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/index.css', function(req, res){
+    res.sendFile(__dirname + '/index.css');
+});
+
 app.get('/index.html', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
